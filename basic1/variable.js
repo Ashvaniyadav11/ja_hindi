791d25b05a3a5d8f2cfe91aa5a4c38739e3c9e44
Ashvaniyadav11 <ashvaniyadav21437@gmail.com>
2025-07-12 07:32:26 +0000

    VARIABLE complete done

---
+++ basic1/variable.js
@@ -0,0 +1,10 @@
+const accountId = 12432234542
+let accountEmail ="ashvaniyadav2321@gmail.com"
+var accountPassowrd ="wedfe143534"
+accountcity = "rajasthan"
+
+//accountId = 8465583938
+ accountEmail = "hrridhkcjdhdh123@gmail.com"
+ accountPassowrd = "hitshsif2344"
+ accountcity = "JAPAN"
+console.table([accountId,accountEmail,accountPassowrd,accountcity] )
