e41733a879446caee28125705428c7d09e0f374e
Ashvaniyadav11 <ashvaniyadav21437@gmail.com>
2025-07-12 19:52:35 +0000

    complete datatypes

---
+++ basic1/datatype.js
@@ -0,0 +1,28 @@
+"use strict" //treat all js code as newer version
+//alert(3+ 3) // we are using  nodejs not brouser
+// datatypes
+// number => 2 to power 55
+//bigint
+//boolean => true or false
+//null => standalone value; it is actually represent EMPTY value;
+// symbol => unique
+// undefind 
+console.log(typeof null);
+// object
+  const user = {
+    name:"ashvani Yadav" ,
+    comapany:"kinguston",
+    age: 19,
+    live: "pratapgarh",
+    gender: "male",
+    post: { 
+        date: 23,
+        nameofpost:"Jai Shree Ram"
+
+    }
+}
+console.log(user);
+// use to write single line comment 
+/* 
+use to write multi line comments
+*/
