b6e996b5b9ac98cf68dad34a6414afa0b88ce9aa
Ashvaniyadav11 <ashvaniyadav21437@gmail.com>
2025-07-13 05:30:17 +0000

    summary of datatype

---
+++ basic1/datatype_summary.js
@@ -0,0 +1,22 @@
+// Primitive (call value )
+
+
+// 7 types: String ,number , boolean, null , undefined , symbol, Bigint
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid);
+
+const bigNumber = 345678997654n // automatically became Bigint 
+
+
+// reference ( Non primitive)
+//Array, OBJECT , functions
+
+
+const heros = ["shaktiman", " neeraj", "my hobby"]
+
+const myFunction = function(){
+    console.log("Ashvani Yadav");
+    
+}
