c8bcb8141f6dc90319cdd5e1201133dfc81d2688
Ashvaniyadav11 <ashvaniyadav21437@gmail.com>
2025-07-13 03:10:06 +0000

    complete conversion

---
+++ basic1/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+ //let scores = "33abs"
+  let scores = "hitesh"
+console.log(typeof scores);
+console.log(typeof (scores));
+
+let valueInnumber = Number(scores)
+console.log( valueInnumber)
+
+// "33" => 33
+//    "33abc  " => NaN
+//true => 1, false => 0
+
+let logged = "Hiteah"
+
+let booleanlogged = Boolean(logged)
+console.log(booleanlogged)
+
+// 1 => true ; 0 => false 
+// "" => false 
+// " " = "Ashvani" => true 
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
